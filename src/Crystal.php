b43c15f7a6c1d42d31b1e83f76e0171c5e81d14e
Tomás Juárez <tom.-95@hotmail.com>
2014-01-06 22:53:24 -0200

    Update Crystal.php

--- src/Crystal.php
+++ src/Crystal.php
@@ -1,12 +1,18 @@
 <?php
 
+namespace Crystal;
+
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'QueryConstructor.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'DataBase.php';
+
+use Crystal\QueryConstructor
+  , Crystal\DataBase 
+  ;
 
 /**
  * Abstraction Layer
  */
-class Crystal extends QueryConstructor
+class main extends QueryConstructor
 {
   
   public function __construct () {
