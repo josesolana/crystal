d19f50731a032cee5f341312cbd706075ee07a37
Tomás Juárez <tom.-95@hotmail.com>
2014-01-08 10:43:28 -0200

    Update QueryConstructor.php

--- src/QueryConstructor.php
+++ src/QueryConstructor.php
@@ -6,8 +6,7 @@ namespace Crystal;
  * This class set all the querys syntax
  * Class QueryConstructor
  */
-class QueryConstructor 
-{
+class QueryConstructor {
   /**
    * Index of where() params
    * WHERE_VALUE_INDEX_MIN and WHERE_VALUE_INDEX are used when where() have 2 arguments or 3, respectivelly
@@ -19,10 +18,7 @@ class QueryConstructor
       WHERE_VALUE_INDEX     = 2
   ;
   
-  private static 
-      $_DB    = null,
-      $_table = ''
-  ;
+  private static $_table = '';
   
   private $_operation = '',
           $_statement = '',
@@ -32,9 +28,7 @@ class QueryConstructor
   ;
   
   
-  public function __construct ( DataBase $db ) {
-    self::$_DB = $db;
-  }
+  public function __construct () { }
   
   public static function setTable ( $table ) {
     if ( ! empty ( $table ) ) {
@@ -45,7 +39,7 @@ class QueryConstructor
   }
   
   /**
-   * Shit matrix
+   * Shift matrix
    * @param array $matrix
    * @param mixed $element
    * @param Integer $index
@@ -137,7 +131,7 @@ class QueryConstructor
         $value   = $arguments [ self::WHERE_VALUE_INDEX ];
         break;
       default :
-        throw new Exception('Function where() must be 2 or 3 parameters');
+        throw new \Exception('Function where() must be 2 or 3 parameters');
         break;
     }
     return Array( $key, $compare, $value );
@@ -159,7 +153,7 @@ class QueryConstructor
     $field      = $arguments [ 0 ];
     $comparison = $arguments [ 1 ];
     $value      = $arguments [ 2 ];
-    $genericKey = $field . round (  rand(1000, 5000) * rand() / rand() );
+    $genericKey = $field . round (  rand ( 1000, 5000 ) * rand() / rand() );
     
     $this->_values = array_merge ( $this->_values, $this->buildAssocValues ( $genericKey, $value ) );
     
@@ -174,8 +168,6 @@ class QueryConstructor
      * Pretty soon the user will be able to use another boolean value
      */
     $this->_hasWhere = true;
-    
-    return $this;
 
   }
   
@@ -194,12 +186,124 @@ class QueryConstructor
     if ( 'select' == $this->_operation ) {
       if ( is_integer( $number ) ) {
         $this->_statement = $this->concat( $this->_statement, 'LIMIT', $number );
-        return $this;
+        return;
       }
       else
         throw new \Exception(__FUNCTION__.' parameter must be a number.');  
     }
     throw new \Exception (__FUNCTION__ . ' must be called after the select() query method');
+  }
+
+  
+  /**
+   * All the database basic operations
+   * @returns \QueryConstructor
+   */
+  
+  public function select ( /*multiple arguments*/ ) {
+ 
+    $this->_operation = 'select';
+    
+    if ( ! self::$_table )
+      throw new \Exception('You must to specify a table using QueryConstructor::setTable(\'table_name\')');
+    
+    if ( ! (boolean) func_num_args() ) 
+      $fields = '*';
+    else 
+      //$fields = $this->concat('(', $this->buildParts ( func_get_args() ), ')');
+      $fields = $this->buildParts ( func_get_args() );
+ 
+    $this->_statement = ( ! $this->_hasWhere ) 
+            ? $this->concat ( 'SELECT',  $fields, 'FROM', self::$_table )
+            : $this->concat ( 'SELECT', $fields, 'FROM', self::$_table, $this->_statement )
+            ;
+  }
+  
+  public function insert () {
+    $this->_operation = 'insert';
+    
+    foreach (func_get_args() as $elements ) {
+      if ( is_array ( $elements ) ) { 
+        
+        $temporalValues = Array ();
+        
+        array_walk( $elements, function ( $value, $key ) use ( &$temporalValues ) {
+          $temporalValues = array_merge ( $temporalValues, $this->buildAssocValues($key, $value) );
+        });
+        
+        $fields = $this->buildParts( array_keys ( $elements ) );
+        $values = $this->buildParts( array_keys ( $elements ), false, true );
+
+        $this->_statement = $this->concat('INSERT INTO', self::$_table, '(', $fields, ')', 'VALUES (', $values, ')');
+        array_push( $this->_values, $temporalValues );
+        
+      }
+      else {
+        throw new Exception('Argumenst must be defined as Array');
+      }
+    }
+  }
+  
+  public function update ( Array $sets ) {
+    $this->_operation = 'update';
+    
+    $set = $this->buildParts( array_keys( $sets ),true, true, false);
+    
+    array_walk($sets, function ( $value, $key ) {
+      $this->_values = array_merge($this->_values, $this->buildAssocValues($key, $value));
+    });
+  
+    $this->_statement = ( ! $this->_hasWhere )
+            ? $this->concat('UPDATE', self::$_table, 'SET', $set)
+            : $this->concat('UPDATE', self::$_table, 'SET', $set);
+    
+  }
+  
+  public function delete (/*multiple params*/) {
+    $this->_operation = 'delete';
+    
+    $params = func_get_args();
+    
+    $fields = null;
+    if ( ! empty ( $params ) ) 
+      $fields = $this->buildParts( $params );
+    
+    $this->_statement = ( ! $this->_hasWhere )
+        ? $this->concat('DELETE', $fields, 'FROM', self::$_table)
+        : $this->concat('DELETE', $fields, 'FROM', self::$_table, $this->_statement);
+    
+  }
+  
+  public function cleanData () {
+    $this->_hasWhere  = false;
+    $this->_values    = Array();
+    $this->_statement = null;
+  }
+  
+  public function getValues () {
+    return $this->_values;
+  }
+ 
+  public function getStatement () {
+    /*
+    if ( 'select' == $this->_operation )
+      $this->_statement = ( $this->_hasWhere ) 
+            ? $this->_statement 
+            : preg_replace ( '/\(|\)/', '', $this->_statement );*/
+    
+    //Otros métodos
+    return $this->_statement;
+  }
+  
+  public function getCurrentOperation () {
+    return $this->_operation;
+  }
+  
+  /**
+   * pendientes
+   */
+  public function truncate () {
+    
   }
   
   public function group () {
@@ -232,164 +336,6 @@ class QueryConstructor
   
   public function raw () {
     
-  }
-  
-  public function transaction () {
-    
-  } 
-  
-  public function processProcedure () {
-    
-  }
-  
-  
-  /**
-   * Execute the statement
-   * @return mixed
-   */
-  public function execute () {
-    
-    switch ( $this->_operation ) {
-      case 'select' :
-        /**
-         * Temporal solution
-         */
-        $this->_statement = ( $this->_hasWhere ) ? $this->_statement : preg_replace ( '/\(|\)/', '', $this->_statement );
-        
-        $execute = self::$_DB->executeSelect ( $this->_statement, $this->_values );
-        if ( func_num_args() > 0 ) {
-          if ( is_callable ( $callback = func_get_arg ( 0 ) ) ) {
-            /**
-             * If $execute is an Array, the query has been procces, otherwise, returns false.
-             * If $execute is false, the second parameter will be a null Array             
-             * 
-             */
-            call_user_func ( $callback, ! is_array ( $execute ), $execute ?: [ ], count ( $execute ) );
-          }
-        }
-        break;
-          
-      case 'insert' :
-        /*Set all the values for the statement*/
-        foreach ( $this->_values as $value ) {
-          if ( ! $execute = self::$_DB->executeInsert ( $this->_statement, $value ) )
-            break;
-        }
-        break;
-        
-      case 'update' :
-        $execute = self::$_DB->executeUpdate ( $this->_statement, $this->_values );
-        break;
-      
-      case 'delete' :
-        $execute = self::$_DB->executeDelete ( $this->_statement, $this->_values );
-        break;      
-    }
-    
-    $this->cleanData();
-    return $execute;
-  }
-  
-  /**
-   * All the database basic operations
-   * @returns \QueryConstructor
-   */
-  
-  public function select ( /*multiple arguments*/ ) {
- 
-    $this->_operation = 'select';
-    
-    if ( ! self::$_table )
-      throw new Exception('You must to specify a table using QueryConstructor::setTable(\'table_name\')');
-    
-    if ( ! (boolean) func_num_args() ) 
-      $fields = '*';
-    else 
-      //$fields = $this->concat('(', $this->buildParts ( func_get_args() ), ')');
-      $fields = $this->buildParts ( func_get_args() );
- 
-    $this->_statement = ( ! $this->_hasWhere ) 
-            ? $this->concat ( 'SELECT',  $fields, 'FROM', self::$_table )
-            : $this->concat ( 'SELECT', $fields, 'FROM', self::$_table, $this->_statement )
-            ;
-    
-    return $this;
-  }
-  
-  public function insert () {
-    $this->_operation = 'insert';
-    
-    foreach (func_get_args() as $elements ) {
-      if ( is_array ( $elements ) ) { 
-        
-        $temporalValues = Array ();
-        
-        array_walk( $elements, function ( $value, $key ) use ( &$temporalValues ) {
-          $temporalValues = array_merge ( $temporalValues, $this->buildAssocValues($key, $value) );
-        });
-        
-        $fields = $this->buildParts( array_keys ( $elements ) );
-        $values = $this->buildParts( array_keys ( $elements ), false, true );
-
-        $this->_statement = $this->concat('INSERT INTO', self::$_table, '(', $fields, ')', 'VALUES (', $values, ')');
-        array_push( $this->_values, $temporalValues );
-        
-      }
-      else {
-        throw new Exception('Argumenst must be defined as Array');
-      }
-    }
-    return $this;
-  }
-  
-  public function update ( Array $sets ) {
-    $this->_operation = 'update';
-    
-    $set = $this->buildParts( array_keys( $sets ),true, true, false);
-    
-    array_walk($sets, function ( $value, $key ) {
-      $this->_values = array_merge($this->_values, $this->buildAssocValues($key, $value));
-    });
-  
-    $this->_statement = ( ! $this->_hasWhere )
-            ? $this->concat('UPDATE', self::$_table, 'SET', $set)
-            : $this->concat('UPDATE', self::$_table, 'SET', $set);
-    
-    return $this;
-  }
-  
-  public function delete (/*multiple params*/) {
-    $this->_operation = 'delete';
-    
-    $params = func_get_args();
-    
-    $fields = null;
-    if ( ! empty ( $params ) ) 
-      $fields = $this->buildParts( $params );
-    
-    $this->_statement = ( ! $this->_hasWhere )
-        ? $this->concat('DELETE', $fields, 'FROM', self::$_table)
-        : $this->concat('DELETE', $fields, 'FROM', self::$_table, $this->_statement);
-    
-    return $this;
-  }
-  
-  public function truncate () {
-    
-  }
-  
-  private function cleanData () {
-    $this->_hasWhere  = false;
-    $this->_values    = Array();
-    $this->_statement = null;
-  }
- 
-  public function getStatement() {
-    return $this->_statement;
-  }
-  
-  public function getValues () {
-    return $this->_values;
   }
   
 }
